1e7e11d9189f35baa87f585fdab805db54e693bb
Leam Hall <leamhall@gmail.com>
2017-09-07 06:21:11 -0400

    Added list_from_file() and random_from_list() to base_tools.py.
    Included tests and sample data file.

--- lib/base_tools.py
+++ lib/base_tools.py
@@ -17,3 +17,41 @@ def roll_2d6():
     () -> int
   """
   return roll_1d6() + roll_1d6()
+
+def modify_attribute(data):
+  """
+  NOTE:  This is untested!!!
+  Takes a dict of an instance, an attribute to modify, a key in that 
+  attribute, and an optional value.
+  Creates the key,value pair with a default if key does not exist.
+  """
+  inst      = data,get('instance')
+  attribute = data.get('attribute')
+  key       = data.get('key', None)
+  value     = data.get('value', 1) 
+ 
+def list_from_file(file):
+  """
+  Takes a file of one list item per line. 
+  Returns a list of those items.
+  Removes whitespace and blank lines.
+  """
+  datafile = open(file, 'r')
+  items = []
+  for line in datafile:
+    line  = line.strip()
+    if len(line) > 0:
+      items.append(line)
+  return items
+
+
+
+def random_from_list(list):
+  """
+  Returns a random item from the list.
+  """
+  return random.choice(list)
+
+if __name__ == '__main__':
+  list_from_file('data.txt')
+
--- tests/test_base_tools.py
+++ tests/test_base_tools.py
@@ -21,6 +21,17 @@ class TestBaseTools(unittest.TestCase):
     self.assertTrue(min(rolls) == 2)
     self.assertTrue(max(rolls) == 12)
 
+  def test_random_from_list(self):
+    data_options = ['Marco', 'Irene', 'Tala', 'Ian', 
+      'Akil', 'Alba', 'Hannah', 'Aldo']
+    item = base_tools.random_from_list(data_options)
+    self.assertTrue(item in data_options)
+  
+  def test_list_from_file(self):
+    data_file = 'tests/data.txt'
+    items = base_tools.list_from_file(data_file)
+    self.assertTrue(len(items) == 8)
+
 
 if __name__ == '__main__':
   unittest.main()
