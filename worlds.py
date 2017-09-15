7d4cce29a61634aa4b2f3ffe2a940d607917c4c7
Leam Hall <leamhall@gmail.com>
2017-09-15 16:43:19 -0400

    Refactored worlds.py.

--- worlds.py
+++ worlds.py
@@ -1,29 +1,18 @@
-""" chargen.py """
+""" worlds.py 
+  python worlds.py [N]
+"""
 
 from __future__ import print_function
-""" Required to use print(rank, new_char) """
 
-import getopt
-
+import argparse
 import sys
 sys.path.append("lib")
 from world_tools import *
 
-def usage():
-    print('usage: worlds [-nh]')
+parser = argparse.ArgumentParser(description='Get number or worlds.')
+parser.add_argument('number', metavar='N', type=int, nargs='?',
+  help='Number of world names.', default=1)
+args = parser.parse_args()
 
-if len(sys.argv) > 1:
-    opts, args = getopt.getopt(sys.argv[1:], 'n:h')
-    for o, a in opts:
-        if o == '-n':
-            if isinstance(a, str) and len(a) > 0:
-                n = int(a)
-                for i in range(n):
-                    print(world_name())
-        sys.exit()
-    
-    if o in ['-h', '--help']:
-        usage()
-        sys.exit()
-
-print(world_name())
+for i in range(args.number):
+  print(world_name())
