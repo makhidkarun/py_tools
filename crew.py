5d37fd2b9a9fe29694f0c594571784f31f0d84a8
Leam Hall <leamhall@gmail.com>
2017-09-15 16:56:57 -0400

    More argparse work.

--- crew.py
+++ crew.py
@@ -1,10 +1,23 @@
-import getopt
+""" crew.py
+  python crew.py -m <'fireteam', 'squad', 'platoon'>
+  python crew.py -s <int ship size>
+"""
+import argparse
 
 import sys
 sys.path.append("lib")
 from character import Character
 
-data = {}
+parser = argparse.ArgumentParser(description='Generate a ship or military crew')
+parser.add_argument('-m', '--military', help='military unit of what size?',
+  default='fireteam')
+
+args = parser.parse_args()
+if 'military' in list(args.keys()):
+  print(args.military)
+
+sys.exit()
+
 if len(sys.argv) > 1:
   opts, args = getopt.getopt(sys.argv[1:], 'm:s:')
   for o, a in opts:
